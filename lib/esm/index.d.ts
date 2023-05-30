57b8c465510ff78d874d6fa571a393e4a7e67389
whoami1337 <mrs4z@icloud.com>
2023-05-31 02:34:37 +0300

    fix

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -1,2 +1,6 @@
 export * from './interfaces/IUsers';
 export declare function sayHello(name: string): string;
+declare const _default: {
+    resultItem: () => void;
+};
+export default _default;
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export * from './interfaces/IUsers';
 export function sayHello(name: string) {
     return `Hey ${name}, say hello to TypeScript.`;
 }
+
+export default {
+    resultItem: () => console.log("FUCK GROM INDEX TSD")
+}
